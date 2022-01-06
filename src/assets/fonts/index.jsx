dc5ff0585e019102483d67fb86c52ef4bbc4e3fd
Jeongs <kimm.jeongs@gmail.com>
2022-01-07 00:57:30 +0900

    :bento:  Assets: colors, fonts 모듈 생성

---
+++ src/assets/fonts/index.jsx
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react'
+import defaultColor from '../colors'
+
+const font = {
+  base: css`
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: ${defaultColor.primary};
+  `,
+}
+
+export default font
