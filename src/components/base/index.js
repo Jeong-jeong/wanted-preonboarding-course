b9db1513cecd8aab043cc737fa76821474ed8d9a
Jeongs <kimm.jeongs@gmail.com>
2022-01-07 17:51:39 +0900

    :sparkles:  Feat: Tag 컴포넌트 구현

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,3 +1,4 @@
 export { default as Badge } from './Badge'
 export { default as IconKeyword } from './IconKeyword'
 export { default as Avatar } from './Avatar'
+export { default as Tag } from './Tag'
