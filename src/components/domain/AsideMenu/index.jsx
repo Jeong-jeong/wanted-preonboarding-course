71a08eeb6a38f4b23723d09a78c3220847219e10
Jeongs <kimm.jeongs@gmail.com>
2022-01-08 02:09:00 +0900

    :sparkles:  Feat: AsideMenu 컴포넌트 구현

--- src/components/domain/AsideMenu/index.jsx
+++ src/components/domain/AsideMenu/index.jsx
@@ -1,7 +1,33 @@
+import { AsideWrapper, AsideList, AsideItem, Divider } from './style'
+import { IconBtn, Tag } from '@components/base'
+import { SM, SMhidden } from '@style/MediaQuery'
+
 const AsideMenu = () => {
-	return (
-		
-	)
+  return (
+    <AsideWrapper>
+      <AsideList>
+        <AsideItem>
+          <IconBtn />
+        </AsideItem>
+        <AsideItem>
+          <IconBtn mode="bell" />
+        </AsideItem>
+        <SM>
+          <AsideItem>
+            <IconBtn mode="menu" />
+          </AsideItem>
+        </SM>
+        <SMhidden>
+          <AsideItem>
+            <IconBtn mode="avatar" />
+          </AsideItem>
+          <AsideItem divider>
+            <Tag label="기업 서비스 바로가기">기업 서비스</Tag>
+          </AsideItem>
+        </SMhidden>
+      </AsideList>
+    </AsideWrapper>
+  )
 }
 
 export default AsideMenu
