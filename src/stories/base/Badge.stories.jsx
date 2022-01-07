892d91a89ea9fa276a78087d00647a13e5732b6a
Jeongs <kimm.jeongs@gmail.com>
2022-01-07 17:54:17 +0900

    :sparkles:  Feat: 업데이트 avatarBadge in Badge 스토리

--- src/stories/base/Badge.stories.jsx
+++ src/stories/base/Badge.stories.jsx
@@ -1,4 +1,4 @@
-import { Badge } from '@components/base'
+import { Badge, Avatar } from '@components/base'
 import { ReactComponent as IconBell } from '@assets/icons/icon-bell.svg'
 
 export default {
@@ -9,8 +9,31 @@ export default {
 export const Default = () => {
   return (
     <ul>
-      <li style={{ position: 'relative', listStyle: 'none', top: 100 }}>
+      <li style={{ position: 'relative', top: 100 }}>
         <IconBell aria-label="알람" />
+        <Badge style={{ top: -4, left: 24 }} />
+      </li>
+    </ul>
+  )
+}
+
+export const AvatarBadge = () => {
+  return (
+    <ul style={{ display: 'flex' }}>
+      <li style={{ position: 'relative' }}>
+        <button
+          style={{
+            padding: '0 5px',
+            margin: '5px 5px 0 0',
+            verticalAlign: 'middle',
+          }}
+        >
+          <IconBell aria-label="알람" />
+        </button>
+        <Badge style={{ top: -4, left: 24 }} />
+      </li>
+      <li style={{ position: 'relative' }}>
+        <Avatar />
         <Badge style={{ top: -4, left: 24 }} />
       </li>
     </ul>
