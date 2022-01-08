428c03cbd61a33f37d1f8b8341dc8c4934def77d
Jeongs <kimm.jeongs@gmail.com>
2022-01-08 17:51:49 +0900

    :sparkles:  Feat: MainNav 컴포넌트 구현

--- src/components/domain/index.js
+++ src/components/domain/index.js
@@ -1,2 +1,3 @@
 export { default as AsideMenu } from './AsideMenu'
 export { default as MainBar } from './MainBar'
+export { default as MainNav } from './MainNav'
---
+++ src/stories/domain/MainNav.stories.jsx
@@ -0,0 +1,8 @@
+import { MainNav } from '@components/domain'
+
+export default {
+  title: 'Domain/MainNav',
+  component: MainNav,
+}
+
+export const Default = () => <MainNav />
