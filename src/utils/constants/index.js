e8b0965199aa09a3735d3b16486475b034548d3e
Jeongs <kimm.jeongs@gmail.com>
2022-01-09 19:12:16 +0900

    :lipstick:  UI: 글로벌 컬러, css, constnats 설정

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,1 +1,12 @@
 export const MAX_WIDTH = '1060px'
+
+// COMPONENT SIZE
+export const GNB_HEIGHT = '50px'
+export const GNB_HEIGHT_SM = '110px'
+export const GNB_PD_HORIZONTAL = '20px'
+export const GNB_PD_VETCITAL = '15px'
+
+export const MAINBAR_HEIGHT = '60px'
+
+// Z-INDEX
+export const GNB_ZINDEX = 100
