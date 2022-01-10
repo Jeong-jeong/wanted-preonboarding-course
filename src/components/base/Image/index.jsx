14d96b315290116c6ca9df3a82bbf2e16c619e5a
Jeongs <kimm.jeongs@gmail.com>
2022-01-10 18:52:52 +0900

    :bug:  Fix: display 렌더 오류 수정

--- src/components/base/Image/index.jsx
+++ src/components/base/Image/index.jsx
@@ -2,11 +2,21 @@ import PropTypes from 'prop-types'
 import { typeChecking } from '@utils/functions'
 import { css } from '@emotion/core'
 
-const Image = ({ block, width, height, src, alt, mode, ...props }) => {
+const Image = ({
+  block,
+  width,
+  height,
+  borderRadius,
+  src,
+  alt,
+  mode,
+  ...props
+}) => {
   const ImageStyle = css`
-    display: ${block} ? 'block' : 'inline-block';
+    display: ${block ? 'block' : 'inline-block'};
     width: ${typeChecking(width)};
     height: ${typeChecking(height)};
+    border-radius: ${borderRadius};
     object-fit: ${mode};
   `
 
@@ -17,6 +27,7 @@ Image.propTypes = {
   block: PropTypes.bool,
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  borderRadius: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   src: PropTypes.string,
   alt: PropTypes.string.isRequired,
   mode: PropTypes.oneOf(['fill', 'cover', 'none', 'contain']),
@@ -27,6 +38,7 @@ Image.defaultProps = {
   block: false,
   width: '100%',
   height: '100%',
+  borderRadius: 0,
   mode: 'fill',
 }
 
